d58a70495a0f8b2739ec9059117a0f81865cbbf1
ClaraHapp <clara.happ@stat.uni-muenchen.de>
2016-06-06 14:39:41 +0200

    fcptpaBasis: uncomment example

--- R/univDecomp.R
+++ R/univDecomp.R
@@ -241,24 +241,24 @@ makeDiffOp <- function(degree, dim){
 #'   
 #' @export fcptpaBasis
 #'   
-# @examples
-# # simulate image data for N = 100 observations
-# N <- 100
-# b1 <- eFun(seq(0,1,0.01), M = 7, type = "Poly")
-# b2 <- eFun(seq(-pi, pi, 0.03), M = 8, type = "Fourier")
-# b <- tensorProduct(b1,b2) # 2D basis functions
-# scores <- matrix(rnorm(N*56), nrow = N)
-# f <- defaultFunction(scores = scores, functions = b) # calculate observation (= linear combination of basis functions)
-# 
-# # calculate basis functions based on FCP_TPA algorithm (needs some time)
-# fcptpa <- fcptpaBasis(f, npc = 5, alphaRange = list(v = c(1e-5, 1e5), w = c(1e-5, 1e5)))
-# 
-# oldpar <- par(no.readonly = TRUE)
-# 
-# for(i in 1:5) # plot all 5 basis functions
-# plot(fcptpa$functions, obs = i, main = paste("Basis function", i)) # plot first basis function
-# 
-# par(oldpar)
+#' @examples
+#' # simulate image data for N = 100 observations
+#' N <- 100
+#' b1 <- eFun(seq(0,1,0.01), M = 7, type = "Poly")
+#' b2 <- eFun(seq(-pi, pi, 0.03), M = 8, type = "Fourier")
+#' b <- tensorProduct(b1,b2) # 2D basis functions
+#' scores <- matrix(rnorm(N*56), nrow = N)
+#' f <- defaultFunction(scores = scores, functions = b) # calculate observation (= linear combination of basis functions)
+#' 
+#' # calculate basis functions based on FCP_TPA algorithm (needs some time)
+#' fcptpa <- fcptpaBasis(f, npc = 5, alphaRange = list(v = c(1e-5, 1e5), w = c(1e-5, 1e5)))
+#' 
+#' oldpar <- par(no.readonly = TRUE)
+#' 
+#' for(i in 1:5) # plot all 5 basis functions
+#' plot(fcptpa$functions, obs = i, main = paste("Basis function", i)) # plot first basis function
+#' 
+#' par(oldpar)
 fcptpaBasis <- function(funDataObject, npc, smoothingDegree = rep(2,2), alphaRange)
 {
   if(dimSupp(funDataObject) != 2)
