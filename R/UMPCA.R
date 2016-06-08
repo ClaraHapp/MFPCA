6eecf43d08abc87106e383665395ca3add49499d
ClaraHapp <clara.happ@stat.uni-muenchen.de>
2016-06-08 11:45:55 +0200

    function ttv now with examples

--- R/UMPCA.R
+++ R/UMPCA.R
@@ -227,6 +227,22 @@ ttvCalculation <- function(A, v, dim)
 #'   32(4):635-653, December 2006.
 #'   
 #' @export ttv
+#' 
+#' @examples
+#' # create a three-mode tensor
+#' a1 <- seq(0,1, length.out = 10)
+#' a2 <- seq(-1,1, length.out = 20)
+#' a3 <- seq(-pi, pi, length.out = 15)
+#' A <-a1 %o% a2 %o% a3
+#' dim(A)
+#' 
+#' # multiply along different dimensions
+#' dim(ttv(A = A, v = list(rnorm(10)), dim = 1))
+#' dim(ttv(A = A, v = list(rnorm(20)), dim = 2))
+#' dim(ttv(A = A, v = list(rnorm(15)), dim = 3))
+#' 
+#' # multiply along more than one dimension
+#' length(ttv(A = A, v = list(rnorm(10), rnorm(15)), dim = c(1,3)))
 ttv <- function(A, v, dim)
 {
   # check input arguments
