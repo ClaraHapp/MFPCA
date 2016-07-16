f934a0ee51ea5b7e8bd9f7019c8ef04e6d737d78
Clara Happ <ClaraHapp@users.noreply.github.com>
2016-07-16 15:55:19 +0200

    test_fftw: check even if fftw3 not installed
    check via try(...) is an error occurs. Check via expect_error() is fftw3 is not installed

--- tests/testthat/test_fftw.R
+++ tests/testthat/test_fftw.R
@@ -12,17 +12,28 @@ test_that("Test fftw: DCT", {
   expect_error(MFPCA:::dct2D(image3D, qThresh = 0.9), "dct2D can handle only 2D images")
   expect_error(MFPCA:::dct3D(image2D, qThresh = 0.9), "dct3D can handle only 3D images")
   
-  fftw2D <- MFPCA:::dct2D(image2D, qThresh = 0.95)
-  expect_equal(length(fftw2D$ind), 510)
-  expect_equal(fftw2D$ind[1:3], c(2,92,102))
-  expect_equal(mean(fftw2D$val), -0.00049897171)
-  expect_equal(fftw2D$val[1], 0.018282019)
+  fftw2D <- try(MFPCA:::dct2D(image2D, qThresh = 0.95), silent = TRUE)
+  if(class(fftw2D) == "try-error")
+    expect_error(stop(fftw2D), "dctBasis2D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.")
+  else
+  {
+    expect_equal(length(fftw2D$ind), 510)
+    expect_equal(fftw2D$ind[1:3], c(2,92,102))
+    expect_equal(mean(fftw2D$val), -0.00049897171)
+    expect_equal(fftw2D$val[1], 0.018282019)
+  }
   
-  fftw3D <- MFPCA:::dct3D(image3D, qThresh = 0.95)
-  expect_equal(length(fftw3D$ind), 10711)
-  expect_equal(fftw3D$ind[1:3], c(102, 103, 110))
-  expect_equal(mean(fftw3D$val), 0.00027522909)
-  expect_equal(fftw3D$val[1], -0.32200299)
+  
+  fftw3D <- try(MFPCA:::dct3D(image3D, qThresh = 0.95), silent = TRUE)
+  if(class(fftw3D) == "try-error")
+    expect_error(stop(fftw3D), "dctBasis3D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.")
+  else
+  {
+    expect_equal(length(fftw3D$ind), 10711)
+    expect_equal(fftw3D$ind[1:3], c(102, 103, 110))
+    expect_equal(mean(fftw3D$val), 0.00027522909)
+    expect_equal(fftw3D$val[1], -0.32200299)
+  }
 })
 
 
@@ -33,19 +44,26 @@ test_that("test univariate DCT 2D", {
   f2 <- funData(argvals = list(x1, x2),
                 X = aperm(replicate(10, outer(x1, cos(pi*x2))+matrix(rnorm(50*75, sd = 0.1), nrow = 50)), c(3,1,2)))
   
-  dct2D <- MFPCA:::dctBasis2D(f2, qThresh = 0.95)
-  expect_equal(dim(dct2D$scores), c(10, 3750))
-  expect_equal(length(dct2D$scores@i),  1880) 
-  expect_equal(dct2D$scores@x[1],  -0.0196469613) 
-  expect_equal(dim(dct2D$B), c(3750, 3750))
-  expect_equal(dct2D$B@x[1], 0.202642367)
-  expect_equal(var(diff(dct2D$B@x)), 0)
-  expect_false(dct2D$ortho)  
-  expect_null(dct2D$functions)
+  dct2D <- try(MFPCA:::dctBasis2D(f2, qThresh = 0.95), silent = TRUE)
+  if(class(dct2D) == "try-error")
+    expect_error(stop(dct2D), "dctBasis2D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.")
+  else
+  {
+    expect_equal(dim(dct2D$scores), c(10, 3750))
+    expect_equal(length(dct2D$scores@i),  1880) 
+    expect_equal(dct2D$scores@x[1],  -0.0196469613) 
+    expect_equal(dim(dct2D$B), c(3750, 3750))
+    expect_equal(dct2D$B@x[1], 0.202642367)
+    expect_equal(var(diff(dct2D$B@x)), 0)
+    expect_false(dct2D$ortho)  
+    expect_null(dct2D$functions)
+    
+    # wrapper function
+    decompDCT2D <- MFPCA:::univDecomp(type = "DCT2D", funDataObject = f2, qThresh = 0.95)
+    expect_equal(decompDCT2D, dct2D)
+  }
   
-  # wrapper function
-  decompDCT2D <- MFPCA:::univDecomp(type = "DCT2D", funDataObject = f2, qThresh = 0.95)
-  expect_equal(decompDCT2D, dct2D)
+  
 })
 
 
@@ -56,19 +74,24 @@ test_that("test univariate DCT 3D", {
   x3 <- seq(0, 0.5, length.out = 20)
   f3 <- funData(argvals = list(x1, x2, x3), X = replicate(20, array(rnorm(10*40*30), dim = c(10, 40, 30))))
   
-  dct3D <- MFPCA:::dctBasis3D(f3, qThresh = 0.95)
-  expect_equal(dim(dct3D$scores), c(10, 23998))
-  expect_equal(length(dct3D$scores@i),  12000) 
-  expect_equal(dct3D$scores@x[1],  -0.071277532) 
-  expect_equal(dim(dct3D$B), c(23998, 23998))
-  expect_equal(dct3D$B@x[1], 0.032251534)
-  expect_equal(var(diff(dct3D$B@x)), 0)
-  expect_false(dct3D$ortho)  
-  expect_null(dct3D$functions)
-  
-  # wrapper function
-  decompDCT3D <- MFPCA:::univDecomp(type = "DCT3D", funDataObject = f3, qThresh = 0.95)
-  expect_equal(decompDCT3D, dct3D)
+  dct3D <- try(MFPCA:::dctBasis3D(f3, qThresh = 0.95), silent = TRUE)
+  if(class(dct3D) == "try-error")
+    expect_error(stop(dct3D), "dctBasis3D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.")
+  else
+  {
+    expect_equal(dim(dct3D$scores), c(10, 23998))
+    expect_equal(length(dct3D$scores@i),  12000) 
+    expect_equal(dct3D$scores@x[1],  -0.071277532) 
+    expect_equal(dim(dct3D$B), c(23998, 23998))
+    expect_equal(dct3D$B@x[1], 0.032251534)
+    expect_equal(var(diff(dct3D$B@x)), 0)
+    expect_false(dct3D$ortho)  
+    expect_null(dct3D$functions)
+    
+    # wrapper function
+    decompDCT3D <- MFPCA:::univDecomp(type = "DCT3D", funDataObject = f3, qThresh = 0.95)
+    expect_equal(decompDCT3D, dct3D)
+  }
 })
 
 
@@ -86,15 +109,25 @@ test_that("Test fftw: IDCT", {
   expect_error(MFPCA:::idct3D(scores = scores, ind = 0:24, dim = c(10, 20, 30)), "Indices must be positive.")
   expect_error(MFPCA:::idct3D(scores = scores, ind = 2000+0:24, dim = c(10, 20, 10)), "Index exceeds image dimensions.")
   
-  idct2D <- MFPCA:::idct2D(scores = scores, ind = sample(200, 25), dim = c(10, 20))
-  expect_equal(dim(idct2D), c(10, 20))
-  expect_equal(mean(idct2D), 0)
-  expect_equal(idct2D[1,1], 1.08970077)
+  idct2D <- try(MFPCA:::idct2D(scores = scores, ind = sample(200, 25), dim = c(10, 20)), silent = TRUE)
+  if(class(idct2D) == "try-error")
+    expect_error(stop(idct2D), "dctBasis2D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.")
+  else
+  {
+    expect_equal(dim(idct2D), c(10, 20))
+    expect_equal(mean(idct2D), 0)
+    expect_equal(idct2D[1,1], 1.08970077)
+  }
   
-  idct3D <- MFPCA:::idct3D(scores = scores, ind = sample(2000, 25), dim = c(10, 20, 10))
-  expect_equal(dim(idct3D), c(10, 20, 10))
-  expect_equal(mean(idct3D), 0)
-  expect_equal(idct3D[1,1,1], 0.877946026)
+  idct3D <- try(MFPCA:::idct3D(scores = scores, ind = sample(2000, 25), dim = c(10, 20, 10)), silent = TRUE)
+  if(class(idct3D) == "try-error")
+    expect_error(stop(idct3D), "dctBasis3D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.")
+  else 
+  {
+    expect_equal(dim(idct3D), c(10, 20, 10))
+    expect_equal(mean(idct3D), 0)
+    expect_equal(idct3D[1,1,1], 0.877946026)
+  }
 })
 
 
@@ -103,15 +136,20 @@ test_that("test univariate IDCT 2D", {
   scores <- sapply(25:1, function(x){rnorm(20, sd = x/25)})
   argvals <- list(seq(0, 1, 0.01), seq(-1, 1, 0.02))
   
-  dct2D <- MFPCA:::dctFunction2D(scores = scores, argvals = argvals)
-  expect_equal(nObs(dct2D), 20)
-  expect_equal(nObsPoints(dct2D),  c(101,101))
-  expect_equal(mean(norm(dct2D)),  2.06938459) 
-  expect_equal(norm(dct2D)[1], 2.45252941)
-  
-  # wrapper function
-  expandDCT2D <- MFPCA:::univExpansion(type = "DCT2D", scores = scores, argvals = argvals, functions = NULL)
-  expect_equal(expandDCT2D, dct2D)
+  dct2D <- try(MFPCA:::dctFunction2D(scores = scores, argvals = argvals), silent = TRUE)
+  if(class(dct2D) == "try-error")
+    expect_error(stop(dct2D), "dctBasis2D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.\"")
+  else
+  {
+    expect_equal(nObs(dct2D), 20)
+    expect_equal(nObsPoints(dct2D),  c(101,101))
+    expect_equal(mean(norm(dct2D)),  2.06938459) 
+    expect_equal(norm(dct2D)[1], 2.45252941)
+    
+    # wrapper function
+    expandDCT2D <- MFPCA:::univExpansion(type = "DCT2D", scores = scores, argvals = argvals, functions = NULL)
+    expect_equal(expandDCT2D, dct2D)
+  }
 })
 
 
@@ -120,13 +158,18 @@ test_that("test univariate IDCT 3D", {
   scores <- sapply(60:1, function(x){rnorm(20, sd = x/25)})
   argvals <- list(seq(0, 1, 0.01), seq(-1, 1, 0.02), seq(-0.5, 0.5, 0.05))
   
-  dct3D <- MFPCA:::dctFunction3D(scores = scores, argvals = argvals)
-  expect_equal(nObs(dct3D), 20)
-  expect_equal(nObsPoints(dct3D),  c(101, 101, 21))
-  expect_equal(mean(norm(dct3D)),  7.57861399) 
-  expect_equal(norm(extractObs(dct3D, obs = 1)), 7.64625103)
-  
-  # wrapper function
-  expandDCT3D <- MFPCA:::univExpansion(type = "DCT3D", scores = scores, argvals = argvals, functions = NULL)
-  expect_equal(expandDCT3D, dct3D)
+  dct3D <- try(MFPCA:::dctFunction3D(scores = scores, argvals = argvals), silent = TRUE)
+  if(class(dct3D) == "try-error")
+    expect_error(stop(dct3D), "dctBasis3D requires C-library fftw3 to be installed. Check http://www.fftw.org/ for more information.")
+  else
+  {
+    expect_equal(nObs(dct3D), 20)
+    expect_equal(nObsPoints(dct3D),  c(101, 101, 21))
+    expect_equal(mean(norm(dct3D)),  7.57861399) 
+    expect_equal(norm(extractObs(dct3D, obs = 1)), 7.64625103)
+    
+    # wrapper function
+    expandDCT3D <- MFPCA:::univExpansion(type = "DCT3D", scores = scores, argvals = argvals, functions = NULL)
+    expect_equal(expandDCT3D, dct3D)
+  }
 })
