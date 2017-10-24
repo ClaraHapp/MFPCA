1372bdcd7e9fa84046a129e10d9f8d6239c2ffb8
C. Happ <clara.happ@stat.uni-muenchen.de>
2017-10-24 18:28:27 +0200

    First attempt for biplot

--- R/MFPCAfit_methods.R
+++ R/MFPCAfit_methods.R
@@ -1,8 +1,15 @@
-biplot.MFPCAfit <- function()
+biplot.MFPCAfit <- function(MFPCAobj, choices = 1:2, ...)
 {
+  if(length(choices) != 2)
+    stop("Length of choices must be 2.")
   
+  if(NCOL(MFPCAobj$scores) < max(choices))
+    stop(paste("Argument choices requires", max(choices), 
+               "scores, MFPCA object contains only", NCOL(MFPCAobj$scores), "."))
   
-  
+  graphics:::plot.default(x = MFPCAobj$scores[, choices], y = NULL, ...)
+    
+  return(invisible)
 }
 
 plot.MFPCAfit <- function()
