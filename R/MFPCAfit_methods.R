909e50d539a1e1e694f091244d470306148a4667
ClaraHapp <clara.happ@stat.uni-muenchen.de>
2017-10-25 23:00:14 +0200

    Biplot first draft

--- R/MFPCAfit_methods.R
+++ R/MFPCAfit_methods.R
@@ -7,8 +7,20 @@ biplot.MFPCAfit <- function(MFPCAobj, choices = 1:2, ...)
     stop(paste("Argument choices requires", max(choices), 
                "scores, MFPCA object contains only", NCOL(MFPCAobj$scores), "."))
   
-  graphics:::plot.default(x = MFPCAobj$scores[, choices], y = NULL, ...)
-    
+  # check for labels, otherwise construct them TODO: rownames for scores in MFPCA
+  lab <- {if(is.null(rownames(MFPCAobj$scores)))
+    1:NROW(MFPCAobj$scores)
+    else
+      rownames(MFPCAobj$scores)}
+  
+  
+  graphics::plot.default(x = MFPCAobj$scores[, choices], y = NULL, type = "n", 
+                         xlab = paste("Scores PC", choices[1]), ylab = paste("Scores PC", choices[2]), ...)
+  graphics::text(x = MFPCAobj$scores[, choices], y = NULL, labels = lab, ...)
+  
+  abline(h = 0, lty = 3, col = "gray")
+  abline(v = 0, lty = 3, col = "gray")
+  
   return(invisible)
 }
 
